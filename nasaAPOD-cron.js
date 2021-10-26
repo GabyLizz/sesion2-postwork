6e6e55e8c367a05adc6c3e21f948b8e083376614
GabyLizz <gabylizz.od@gmail.com>
2021-10-25 21:58:13 -0500

    1 file modified

--- nasaAPOD-cron.js
+++ nasaAPOD-cron.js
@@ -20,23 +20,15 @@ function getAPOD() {
         console.log('\n Explicación', body.explanation);
         console.log('\n URL:', body.url);
 
-        return body;
+        fs.writeFileSync(
+          path.join(__dirname, '/ejemplo_1.txt'),
+          'Este es el contenido de mi archivo',
+          { encoding: 'utf-8' }
+        );
+        console.log('writeFileSync:', 'Archivo creado!');
       });
     })
     .on('error', (err) => {
       console.log('Error: ' + err.message);
     });
 }
-
-const createImage = async () => {
-  try {
-    datos = getAPOD().body;
-    await writeFile(datos.title, 'img', JSON.stringify(datos.url));
-    console.log('imagen guardada correctamente');
-  } catch (err) {
-    if (err.isTtyError) throw err;
-    console.log(err);
-  }
-};
-
-createImage();
